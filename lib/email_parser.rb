e9c2fbd54c091fc43ab0d2203bbd88d373b5885f
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-25 02:06:07 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,43 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+require 'pry'
+
+# ("avi@test.com, arel@test.com").parse)
+# (["avi@test.com", "arel@test.com"])
+#     end
+
+#     it "parses space delimited emails" do
+#       expect(EmailAddressParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
+#     end
+
+#     it "parses both CSV and space delimited emails" do
+#       emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
+#       expect(EmailAddressParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com","test@avi.com", "test@arel.com"]) 
+#     end
+
+#     it 'parses and removes duplicate emails' do
+#       expect(EmailAddressParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
+#     end
+
+#split strings to Array
+#join array into 1
+
+class EmailAddressParser
+  
+  attr_accessor :email
+  def initialize(string_of_emails)
+    @email = string_of_emails
+  end  
+  
+  def parse
+    @email = @email.split()
+    @email.each do |mail|
+      if mail.end_with?(',') #|| @email.end_with?(' ')
+        mail[-1] = "" 
+        mail = mail.strip
+        # binding.pry
+      else
+        mail = mail.strip
+      end 
+    end
+    return @email.uniq
+  end
+end
